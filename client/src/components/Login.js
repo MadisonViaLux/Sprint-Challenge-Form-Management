485069057834ff86a5af821bc82f9f05c0b2d000
MadisonViaLux <madison.s.mckown@gmail.com>
2019-07-19 09:06:56 -0700

    Login input fields made with formik

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,40 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
-import * as Yup from "yup";
-import {axiosAuth} from './axiosAuth';
+// import * as Yup from "yup";
+// import {axiosAuth} from './axiosAuth';
 
 
 function Login ({touched, errors, isSubmitting}){
 
     return(
         <Form>
-            
+            <div>
+                <label>User's Name</label>
+
+                <Field 
+                name='username'
+                type='text'
+                autoComplete='off' 
+                />
+
+                <h3>{touched.username && errors.username}</h3>
+            </div>
+
+            <div>
+                <label>User's Password</label>
+
+                <Field 
+                name='password'
+                type='password'
+                autoComplete='off' 
+                />
+
+                <h3>{touched.password && errors.password}</h3>
+            </div>
+
         </Form>
     )
 }
+
+
+export default Login
